29d015341ed0e36de36b6e8d73d4a7fa1ec76a6b
Miquido Ops <mqtc@miquido.com>
2021-06-16 08:34:05 +0000

    chore(deps): bump vpc from 0.25.0 to 0.26.0
    Bumps [vpc](https://github.com/cloudposse/terraform-aws-vpc) from 0.25.0 to 0.26.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-vpc/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-vpc/compare/tags/0.25.0...tags/0.26.0)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                         = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.25.0"
+  source                         = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.26.0"
   name                           = var.name
   namespace                      = var.project
   stage                          = var.environment
